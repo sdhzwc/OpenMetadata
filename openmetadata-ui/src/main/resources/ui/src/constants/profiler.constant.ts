d91dd92bda0d74da5080105dd3020624e14540fd
wuchao <sdhzwc@163.com>
2025-01-14 15:46:16 +0800

    优化洞察时间显示

--- openmetadata-ui/src/main/resources/ui/src/constants/profiler.constant.ts
+++ openmetadata-ui/src/main/resources/ui/src/constants/profiler.constant.ts
@@ -402,7 +402,7 @@ export const TIME_BASED_PARTITION = [
 
 export const TEST_CASE_TYPE_OPTION = [
   ...map(TestCaseType, (value) => ({
-    label: capitalize(value),
+    label: t('label.' + capitalize(value).toLowerCase()),
     value: value,
   })),
 ];
@@ -413,7 +413,7 @@ export const TEST_CASE_STATUS_OPTION = [
     value: '',
   },
   ...values(TestCaseStatus).map((value) => ({
-    label: value,
+    label: t('label.' + value.toLowerCase()),
     value: value,
   })),
 ];
